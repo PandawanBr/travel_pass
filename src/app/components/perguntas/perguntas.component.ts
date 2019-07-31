44d5d79e69822b7ea2640dab2332d90b04f32c51
Rodrigo Kobayashi <rodrigo_kobayashi@outlook.com>
2019-07-31 00:24:27 -0300

    Stepper

--- src/app/components/perguntas/perguntas.component.ts
+++ src/app/components/perguntas/perguntas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-perguntas',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerguntasComponent implements OnInit {
 
-  constructor() { }
+  formGroup1: FormGroup;
+  formGroup2: FormGroup;
+  formGroup3: FormGroup;
 
-  ngOnInit() {
+
+  constructor(
+    private fb: FormBuilder
+  ) {
   }
 
+  ngOnInit() {
+    this.createForm1();
+    this.createForm2();
+    // this.createForm3();
+  }
+
+  createForm1() {
+    this.formGroup1 = this.fb.group({
+      pergunta1: [false],
+      pergunta2: [false],
+      pergunta3: [false],
+      pergunta4: [false],
+      pergunta5: [false],
+      pergunta6: [false],
+      pergunta7: [false],
+      pergunta8: [false],
+      pergunta9: [false],
+      pergunta10: [false],
+      pergunta11: [false],
+      pergunta12: [false],
+    });
+  }
+
+  createForm2() {
+    this.formGroup2 = this.fb.group({
+      pergunta13: [false],
+      pergunta14: [false],
+      pergunta15: [false],
+      pergunta16: [false],
+      pergunta17: [false],
+      pergunta18: [false],
+      pergunta19: [false],
+      pergunta20: [false],
+      pergunta21: [false],
+      pergunta22: [false],
+      pergunta23: [false],
+      pergunta24: [false],
+    });
+  }
+
+  // createForm3() {
+  // this.formGroup1 = this.fb.group({
+  // pergunta1: [false],
+  // pergunta2: [false],
+  // pergunta3: [false],
+  // pergunta4: [false],
+  // pergunta5: [false],
+  // pergunta6: [false],
+  // pergunta7: [false],
+  // pergunta8: [false],
+  // pergunta9: [false],
+  // pergunta10: [false],
+  // pergunta11: [false],
+  // pergunta12: [false],
+  // });
+  // }
 }
