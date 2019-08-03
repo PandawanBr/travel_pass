8627ff2ce79d6c39a23ff0ef85e01e920749b288
Rodrigo Kobayashi <rodrigo_kobayashi@outlook.com>
2019-08-02 22:52:17 -0300

    Ajustando layout

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -22,13 +22,23 @@ export class ListComponent implements OnInit {
   homePage() {
     this.route.navigate(['/']);
   }
+
+  showTrip(value) {
+    window.open(value.image, '_blank');
+  }
 }
 
 const LIST_TRIPS = [
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
-  {name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York'},
+  { name: 'New York', image: '../../../assets/newYork.jpeg', value: 1700.00, description: 'Voo de ida e volta até Nova York' },
+  { name: 'Rio de Janeiro', image: '../../../assets/rio.jpg', value: 450.00, description: 'Voo de ida e volta até o Rio de Janeiro' },
+  {
+    name: 'Porto de Galinhas', image: '../../../assets/portoGalinhas.jpg', value: 2700.00,
+    description: 'Voo de ida e volta até Porto de Galinhas com hotel'
+  },
+  {
+    name: 'Peru', image: '../../../assets/laguna69.jpg', value: 4700.00,
+    description: 'Voo de ida e volta, com hotel e trilha até Laguna 69'
+  },
+  { name: 'Paris', image: '../../../assets/paris.jpg', value: 2600.00, description: 'Voo de ida e volta até Paris' },
+  { name: 'Roma', image: '../../../assets/roma.jpg', value: 5200.00, description: 'Voo de ida e volta até Roma, com visita ao Coliseu' },
 ];
